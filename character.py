91fca6fa5c737b908882b14b9a62ef252d7bef9e
Aman Gopal <AGopal@de.alten.com>
2022-01-21 06:59:23 +0100

    Extend Character class, Use new subclasses to add new functionality to the game

--- character.py
+++ character.py
@@ -2,7 +2,8 @@
 Creating a Character class and also extending it to Enemy and Friend class
 '''
 
-class Character():
+
+class Character:
 
     # Create a character
     def __init__(self, char_name, char_description):
@@ -13,8 +14,8 @@ class Character():
     # Describe this character
     def describe(self):
         print("")
-        print( self.name + " is here!" )
-        print( self.description )
+        print(self.name + " is here!")
+        print(self.description)
 
     # Set what this character will say when talked to
     def set_conversation(self, conversation):
@@ -32,3 +33,40 @@ class Character():
         print(self.name + " doesn't want to fight with you")
         return True
 
+
+class Enemy(Character):
+
+    def __init__(self, char_name, char_description):
+        super().__init__(char_name, char_description)
+        self.weakness = None
+
+    def get_weakness(self):
+        return self.weakness
+
+    def set_weakness(self, char_weakness):
+        self.weakness = char_weakness
+
+    # Override the 'fight()' method of the base class 'Character'
+    def fight(self, combat_item):
+        if self.weakness == combat_item:
+            print("You fend off " + self.name + " with the " + combat_item)
+            return True
+        else:
+            print(self.name + " crushed you, puny adventurer!")
+            return False
+
+
+class Friend(Character):
+
+    def __init__(self, char_name, char_description):
+        super().__init__(char_name, char_description)
+        self.gift = None
+
+    def get_gift(self):
+        return self.gift
+
+    def receive_gift(self):
+        print(self.name + " gifted you " + self.gift + "!!!")
+
+    def set_gift(self, char_gift):
+        self.gift = char_gift
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from room import Room
-from character import Character
+from character import Character, Enemy, Friend
 
 # Initializing objects (rooms) using Room class
 kitchen = Room("Kitchen")
@@ -24,6 +24,18 @@ emily = Character("Emily", "A friendly attendant")
 emily.set_conversation("Would you like to have a drink?")
 ballroom.set_character(emily)  # Placing Character object in Ballroom
 
+# Creating an Enemy object (inherited from Character class) and placing it in a room
+dave = Enemy("Dave", "A smelly zombie")
+dave.set_conversation("I would love to eat your insides...Aaaarrrgh")
+dave.set_weakness("magic")
+dining_hall.set_character(dave)
+
+# Creating a Friend object (inherited from Character class) and placing it in a room
+jack = Friend("Jack", "A jubilant detective")
+jack.set_conversation("Be careful in the Dining Hall!!!")
+jack.set_gift("Torchlight")
+kitchen.set_character(jack)  # Placing Friend object in Ballroom
+
 # Displaying room details
 current_room = kitchen
 current_room.get_details()
@@ -32,7 +44,8 @@ inhabitant = current_room.get_character()
 if inhabitant is not None:
     inhabitant.describe()
 
-while True:
+dead = False
+while not dead:
     print("\n")
     command = input("> ")
     # Command to move between rooms
@@ -47,4 +60,23 @@ while True:
     elif command == "talk":
         if inhabitant is not None and isinstance(inhabitant, Character):
             inhabitant.talk()
+    # Command to fight the Character present in current room, provided it is an Enemy
+    elif command == "fight":
+        if inhabitant is not None and isinstance(inhabitant, Enemy):
+            print("What will you fight with?")
+            fight_with = input()
+            if not inhabitant.fight(fight_with):
+                print("The game has ended!!!")
+                dead = True  # results in 'while' loop and as a result, the game ending
+        else:
+            print("There is no character here to fight with!!!")
+    # Command to receive a gift from the Character present in current room, provided it is a Friend
+    elif command == "gift":
+        if inhabitant is not None:
+            if isinstance(inhabitant, Friend) and inhabitant.get_gift() is not None:
+                inhabitant.receive_gift()
+            elif isinstance(inhabitant, Friend) and inhabitant.get_gift() is None:
+                print("This Friend has no gift sadly!")
+            else:
+                print("This character is not a Friend!")
 
